da095c693a3d2a6fdcc796ff35c2131670aabbb6
ZTARMOBILE\William.Bawwab <william.bawwab@pavocom.com>
2022-12-22 13:19:00 +0200

    GDW-239 #time 3h #comment Implemented the new design for the why GoodMobile page.

--- src/app/support/why-goodmobile/why-goodmobile.component.ts
+++ src/app/support/why-goodmobile/why-goodmobile.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PLANS_SHOP_ROUTE_URLS, SHOP_ROUTE_URLS, SUPPORT_ROUTE_URLS } from '../../app.routes.names';
 import { MetaService } from '../../../services/meta-service.service';
-import { ContentfulService } from '../../../services/contentful.service';
 
 @Component({
   selector: 'app-why-goodmobile',
@@ -12,22 +11,9 @@ export class WhyGoodMobileComponent implements OnInit {
   public SHOP_ROUTE_URLS = SHOP_ROUTE_URLS;
   public SUPPORT_ROUTE_URLS = SUPPORT_ROUTE_URLS;
   public PLANS_SHOP_ROUTE_URLS = PLANS_SHOP_ROUTE_URLS;
-  public whyGoodmobile;
-  public page: any;
-  public innerWidth: any;
-  public images: Array<string>;
-  constructor(private metaService: MetaService,
-              private contentful: ContentfulService) {
-    this.images = ['/assets/img/banners/bring-phone.svg', '/assets/img/banners/nationwide-coverage.svg', 
-    '/assets/img/banners/high-speed.svg', '/assets/img/banners/save-money.svg'];
-  }
+
+  constructor(private metaService: MetaService) { }
   ngOnInit(): void {
     this.metaService.createCanonicalUrl();
-    this.innerWidth = window.innerWidth;
-    this.whyGoodmobile = this.contentful.getContent('whyGood2go');
-  }
-  @HostListener('window:resize', ['$event'])
-  onResize(event): void {
-    this.innerWidth = window.innerWidth;
   }
 }
