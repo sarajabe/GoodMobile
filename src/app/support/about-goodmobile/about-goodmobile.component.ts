df5af63bd517dfc7b757bfd17b53119c9bf09172
ZTARMOBILE\William.Bawwab <william.bawwab@pavocom.com>
2022-12-22 16:12:59 +0200

    GDW-238 #time 2h #comment Implemented the new design for the about GoodMobile page.

--- src/app/support/about-goodmobile/about-goodmobile.component.ts
+++ src/app/support/about-goodmobile/about-goodmobile.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit, HostListener} from '@angular/core';
-import { Router } from '@angular/router';
-import { PLANS_SHOP_ROUTE_URLS, SHOP_ROUTE_URLS, SUPPORT_ROUTE_URLS } from 'src/app/app.routes.names';
+import { Component, OnInit } from '@angular/core';
 import { MetaService } from 'src/services/meta-service.service';
-import { ContentfulService } from 'src/services/contentful.service';
 
 @Component({
   selector: 'app-about-goodmobile',
@@ -10,26 +7,11 @@ import { ContentfulService } from 'src/services/contentful.service';
   styleUrls: ['./about-goodmobile.component.scss']
 })
 export class AboutGoodMobileComponent implements OnInit {
-  public SHOP_ROUTE_URLS = SHOP_ROUTE_URLS;
-  public SUPPORT_ROUTE_URLS = SUPPORT_ROUTE_URLS;
-  public PLANS_SHOP_ROUTE_URLS = PLANS_SHOP_ROUTE_URLS;
-  public page: any;
-  public innerWidth: any;
-  public aboutGoodmobile: any;
-  public readMore = false;
+  public viewContent = false;
 
-  constructor(private router: Router, private metaService: MetaService,
-              private contentful: ContentfulService) {
-  }
+  constructor(private metaService: MetaService) { }
 
-ngOnInit(): void {
-  this.metaService.createCanonicalUrl();
-  this.innerWidth = window.innerWidth;
-  this.aboutGoodmobile = this.contentful.getContent('aboutGood2go');
-}
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event): void {
-    this.innerWidth = window.innerWidth;
+  ngOnInit(): void {
+    this.metaService.createCanonicalUrl();
   }
 }
