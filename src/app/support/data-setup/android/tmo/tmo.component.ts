3ab56cbabf8315cac1bf0d9cab863210513c0ea2
RanaJHaddad <rana.haddad@ztarmobile.com>
2023-01-13 10:12:29 +0300

    GDW-356 #test #time 2h #comment implement android data setup page

--- src/app/support/data-setup/android/tmo/tmo.component.ts
+++ src/app/support/data-setup/android/tmo/tmo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ACCOUNT_ROUTE_URLS, SUPPORT_ROUTE_URLS } from 'src/app/app.routes.names';
 
 @Component({
   selector: 'app-android-tmo',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tmo.component.scss']
 })
 export class AndroidTmoComponent implements OnInit {
+  public ACCOUNT_ROUTE_URLS = ACCOUNT_ROUTE_URLS;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+  public goToDataSetup(): void {
+    this.router.navigate([`${SUPPORT_ROUTE_URLS.BASE}/${SUPPORT_ROUTE_URLS.DATA_SETUP}`]);
   }
 
 }
