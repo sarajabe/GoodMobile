e9063c0d6ff5d36bf40d56307fb9d0e3ca2f4032
mirna.haddad@pavocom.com <mirna.haddad@ztarmobile.com>
2023-03-24 11:32:45 +0300

    GDW-525 #time 1h 30m #comment implement the desktop view for the catalog devices

--- src/app/shop/acp-devices/acp-devices.component.ts
+++ src/app/shop/acp-devices/acp-devices.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CatalogCoreService } from '@ztarmobile/zwp-service-backend-v2';
+import { AppState } from 'src/app/app.service';
 
 @Component({
   selector: 'app-acp-devices',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acp-devices.component.scss']
 })
 export class AcpDevicesComponent implements OnInit {
-
-  constructor() { }
+  public acpDevices = [];
+  constructor(private catalogServices: CatalogCoreService,
+    private appState: AppState) { }
 
   ngOnInit(): void {
+    this.getAcpDevices();
   }
+  public selectDevice(): void {
 
+  }
+  private getAcpDevices(): void {
+    this.appState.loading = true;
+    this.catalogServices.getAcpDevicesFromContentful().then(res => {
+      if (!!res) {
+        this.appState.loading = false;
+        this.acpDevices = res;
+        console.log('acppp', this.acpDevices)
+      }
+    }, error => {
+      this.appState.loading = false;
+    })
+  }
 }
