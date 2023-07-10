805450346f755dd7f836cc7f462c8de027850267
ZTARMOBILE\William.Bawwab <william.bawwab@pavocom.com>
2023-07-10 20:35:00 +0300

    GDW-796 #time 4h #comment Implement the unit testing of the ACP validation component.

---
+++ src/mocks/user-profile.ts
@@ -0,0 +1,46 @@
+import { IUser } from "@ztarmobile/zwp-service-backend";
+
+export const USER: IUser = {
+    customerId: 'A6426970504',
+    email: 'mirna.haddad@pavocom.com',
+    firstName: 'Mirna',
+    id: 'zRIcMhANxvSmKnWTu7MJ7kEQ43u1',
+    lastName: 'Haddad',
+    paymentMethods: [
+        {
+            address1: '800 North 23rd Street',
+            address2: '',
+            alias: 'VISA Ending in 1111',
+            brand: 'VISA',
+            city: 'Philadelphia',
+            country: 'United States',
+            defaultCreditCard: false,
+            expirationDate: '0430',
+            fullName: 'Mirna Y haddad',
+            id: '666114721020220224',
+            isDefault: true,
+            last4: 1234,
+            lastUpdate: { date: 1645721456215 },
+            method: 'Credit Card',
+            name: 'rashed rabadi',
+            postalCode: '19130',
+            state: 'PA'
+        }
+    ],
+    shippingAddresses: [
+        {
+            address1: '800 North 23rd Street',
+            address2: '',
+            city: 'Philadelphia',
+            country: 'United States',
+            id: '-MwfQeFE_JwD6NDxhPhp',
+            isDefault: true,
+            name: 'Mirna haddad',
+            alias: 'Mirna haddad',
+            postalCode: '19130',
+            state: 'PA'
+        }
+    ],
+    verified: false,
+    fullName: 'Mirna Haddad'
+}
