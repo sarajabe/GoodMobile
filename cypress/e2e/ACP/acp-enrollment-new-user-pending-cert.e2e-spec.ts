a4987f7d7f20d3721b713db88ab5f76c6e83551f
Sara_Jaber26 <sara.jaber@pavocom.com>
2023-08-24 15:31:50 +0300

    GDW-802 #comment Implement a test case for signing up a user then purchase an ACP plan No Flow - Pending Certification

---
+++ cypress/e2e/ACP/acp-enrollment-new-user-pending-cert.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { PageObjects } from '../../support/pageObjects';
+import { CONSTANT } from '../../fixtures/constants'
+describe('Sign in then enroll in ACP plan - new user - pending certification - assert ACP summary', () => {
+    before(() => {
+        PageObjects.BeforeAll.executeBeforeAll();
+    });
+    after(() => {
+        PageObjects.AccessControl.logoutFromAccount();
+    });
+    it('Should signup a new acp user', () => {
+        PageObjects.AccessControl.newUserAcp();
+    });
+    it('Should enroll a new user acp pending certification', () => {
+        PageObjects.Acp.enrollmentNewUserAcpPendingCertification();
+    });
+});
