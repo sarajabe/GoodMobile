12122e7899a0095437f943e0525b8501b319b716
Sara_Jaber26 <sara.jaber@pavocom.com>
2023-10-13 18:55:55 +0300

    GDW-944 #comment Update the implementation of e2e testing for Manage device and Sim

--- cypress/e2e/Manage-Device-And-Sim/change-device.e2e-spec.ts
+++ cypress/e2e/Manage-Device-And-Sim/change-device.e2e-spec.ts
@@ -15,7 +15,7 @@ describe('Change device from manage device and sim page ', () => {
         PageObjects.TitleExpectations.goToLogInPage();
     });
     it(`Should fill in fields with existing customer email and password`, () => {
-        PageObjects.AccessControl.logIn(CONSTANT.ACCESS.USER_G_ACCOUNT.EMAIL, CONSTANT.ACCESS.USER_G_ACCOUNT.PASSWORD);
+        PageObjects.AccessControl.logIn(CONSTANT.ACCESS.TEST_USER.EMAIL, CONSTANT.ACCESS.TEST_USER.PASSWORD);
     });
     it('Should click on login button', () => {
         PageObjects.AccessControl.logInButton();
@@ -29,8 +29,9 @@ describe('Change device from manage device and sim page ', () => {
     it('Should go to manage devices page', () => {
         PageObjects.TitleExpectations.goToManageDevicesPage();
     });
-    it('click on i have a new phone btn', () => {
-        PageObjects.ManageDevices.clickOnChangePhone();
+    //
+    it('click on check another device', () => {
+        PageObjects.ManageDevices.clickOnCheckAnotherDevice();
     });
     // check compatiility  valid imei - invalid address
     it('fill in valid IMEI and  invalid address ref', () => {
@@ -43,6 +44,9 @@ describe('Change device from manage device and sim page ', () => {
     it(`Should click on validate btn `, () => {
         PageObjects.ManageDevices.clickOnValidate();
     });
+    it(`Should assert validation message`, () => {
+        cy.get('#required-address-msg').should('have.text','Please select address from the autocomplete list');
+    });
     it('Should stay in the page', () => {
         PageObjects.TitleExpectations.goToManageDevicesPage();
     });
@@ -54,11 +58,8 @@ describe('Change device from manage device and sim page ', () => {
     it(`Should check the invisible recaptcha `, () => {
         PageObjects.Recaptcha.invisibleRecaptcha();
     });
-    it(`Should click on check phone btn `, () => {
-        cy.get('[data-cy="validationMsgInvalidIMEI"]').should('have.text', ' Field should be filled out with correct IMEI/MEID Number. ');
-    });
-    it(`Should check that the validate btn is disabled`, () => {
-        cy.get('[data-cy="validateBtn"]').should('be.disabled');
+    it(`Should assert validation message`, () => {
+        cy.get('[data-cy="validationMsgInvalidIMEI"]').should('have.text', ' Field should be filled out with correct IMEI Number. ');
     });
     it('Should stay in the page', () => {
         PageObjects.TitleExpectations.goToManageDevicesPage();
@@ -71,17 +72,16 @@ describe('Change device from manage device and sim page ', () => {
     it(`Should check the invisible recaptcha `, () => {
         PageObjects.Recaptcha.invisibleRecaptcha();
     });
-    it(`Should check that the validate btn is disabled`, () => {
-        cy.get('[data-cy="validationMsgInvalidIMEI"]').should('have.text', ' Field should be filled out with correct IMEI/MEID Number. ');
-        cy.get('[data-cy="validateBtn"]').should('be.disabled');
+    it(`Should assert validation message`, () => {
+        cy.get('[data-cy="validationMsgInvalidIMEI"]').should('have.text', ' Field should be filled out with correct IMEI Number. ');
     });
     it('Should stay in the page', () => {
         PageObjects.TitleExpectations.goToManageDevicesPage();
     });
     // Enter valid compatibility data
     it('fill in IMEI and address ref', () => {
-        PageObjects.Compatibility.enterIMEInumber(CONSTANT.COMPATIBILITY.IMEIS.IMEI_TMO_HTC);
-        PageObjects.Compatibility.enterAddressRef();
+        PageObjects.Compatibility.enterIMEInumber(CONSTANT.COMPATIBILITY.TMO_ONLY.TMO1);
+        PageObjects.Compatibility.enterTmoAddressRef();
     });
     it('Should check recaptcha', () => {
         PageObjects.Recaptcha.invisibleRecaptcha();
@@ -89,8 +89,8 @@ describe('Change device from manage device and sim page ', () => {
     it('click on validate btn', () => {
         PageObjects.ManageDevices.clickOnValidate();
     });
-    it('click on ok btn', () => {
-        PageObjects.ManageDevices.clickOnOkBtn();
+    it('click on got it btn', () => {
+        PageObjects.ManageDevices.clickOnGotItBtn();
     });
     it('Should stay in same page page', () => {
         PageObjects.TitleExpectations.goToManageDevicesPage();
